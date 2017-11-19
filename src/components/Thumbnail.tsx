ddc1fe11e2af1a9041dba507846534eb7edaf108
Tyson Cadenhead <tysoncadenhead@gmail.com>
2017-11-18 23:14:50 -0600

    First talking walkthough

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,43 +1,58 @@
+import * as R from 'ramda';
 import * as React from 'react';
 
 import { CoffeeCup, Container, Ingredient } from '../styles/ThumbnailStyles';
+import { compose, withHandlers, withState } from 'recompose';
 
+import { componentDidMount } from 'react-functional-lifecycle';
 import findOneCoffee from '../api/findOneCoffee';
 import findOneIngredient from '../api/findOneIngredient';
 
-class Thumbnail extends React.Component <any, any> {
-
-    constructor (props) {
-        super(props);
-
-        this.startHovering = this.startHovering.bind(this);
-        this.endHovering = this.endHovering.bind(this);
-
-        this.state = {
-            coffee: null,
-            hovering: false
-        };
+const Thumbnail = props => {
+    if (!props.coffee) {
+        return null;
     }
+    
+    return (
+        <Container 
+            onMouseDown={props.startHovering}
+            onMouseUp={props.endHovering}
+            onClick={() => props.setSelectedCoffee(props.id)}
+            style={{
+                backgroundColor: props.isFavorite ? '#ffe500' : 'rgb(60, 26, 17)',
+                borderColor: props.isFavorite ? '#FFFFFF' : 'transparent',
+                opacity: props.id === props.selectedCoffee ? 1 : 0.5
+            }}
+        >
+            <CoffeeCup>
+                {props.coffee.ingredients.map(ingredient => (
+                    <Ingredient key={ingredient.id} style={{
+                        backgroundColor: ingredient.color,
+                        color: ingredient.textColor || '#000000',
+                        height: ingredient.oz * 50
+                    }} />
+                ))}
+            </CoffeeCup>
+        </Container>
+    )
+};
 
-    startHovering () {
-        this.setState({
-            hovering: true
-        });
-    }
-
-    endHovering () {
-        this.setState({
-            hovering: false
-        });
-    }
-
-    componentDidMount () {
-        findOneCoffee(this.props.id)
+export default compose(
+    withState('coffee', 'setCoffee', null),
+    withState('hovering', 'setHovering', false),
+    withHandlers({
+        startHovering: R.curry((props, _e) => {
+            props.setHovering(true)
+        }),
+        endHovering: R.curry((props, _e) => {
+            props.setHovering(false)
+        })
+    }),
+    componentDidMount(props => {
+        findOneCoffee(props.id)
             .then(coffee => {
 
-                this.setState({
-                    coffee
-                });
+                props.setCoffee(coffee);
 
                 const ingredientPromises = coffee.ingredients.map((ingredient, key) => {
                     return findOneIngredient(ingredient.id)
@@ -50,44 +65,8 @@ class Thumbnail extends React.Component <any, any> {
                 });
 
                 return Promise.all(ingredientPromises).then(() => {
-                    this.setState({
-                        coffee
-                    });
+                    props.setCoffee(coffee);
                 });
             });
-    }
-
-    render () {
-        if (!this.state.coffee) {
-            return null;
-        }
-        
-        return (
-            <Container 
-                onMouseDown={this.startHovering}
-                onMouseUp={this.endHovering}
-                onClick={this.props.onClick} 
-                style={{
-                    backgroundColor: this.props.isFavorite ? '#ffe500' : 'rgb(60, 26, 17)',
-                    borderColor: this.props.isFavorite ? '#FFFFFF' : 'transparent',
-                    opacity: this.props.id === this.props.selectedCoffee ? 1 : 0.5
-                }}
-            >
-                <CoffeeCup>
-                    {this.state.coffee.ingredients.map(ingredient => (
-                        <Ingredient key={ingredient.id} style={{
-                            backgroundColor: ingredient.color,
-                            color: ingredient.textColor || '#000000',
-                            height: ingredient.oz * 50
-                        }} />
-                    ))}
-                </CoffeeCup>
-            </Container>
-        )
-    }
-
-}
-
-
-
-export default Thumbnail;
+    })
+)(Thumbnail);
