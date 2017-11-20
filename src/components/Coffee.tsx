6de6947ab65a66b723cabeb214a502979fe9110e
Tyson Cadenhead <tysoncadenhead@gmail.com>
2017-11-20 07:15:16 -0600

    Third try

--- src/components/Coffee.tsx
+++ src/components/Coffee.tsx
@@ -2,90 +2,87 @@ import * as React from 'react';
 import * as classnames from 'classnames';
 
 import { CoffeeCup, CoffeeDescription, CoffeeName, Container, CupHandle, Favorite, Ingredient } from '../styles/CoffeeStyles';
+import { compose, withState } from 'recompose';
 
+import { componentDidMount } from 'react-functional-lifecycle';
 import findOneCoffee from '../api/findOneCoffee';
 import findOneIngredient from '../api/findOneIngredient';
 
-class Coffee extends React.Component <any, any> {
-
-    constructor (props) {
-        super(props);
-
-        this.state = {
-            coffee: null
-        };
-    }
-
-    componentDidMount () {
-        if (!this.props.id) {
-            return;
-        }
-    
-        findOneCoffee(this.props.id)
-            .then(coffee => {
-
-                this.setState({
-                    coffee
-                });
-
-                const ingredientPromises = coffee.ingredients.map((ingredient, key) => {
-                    return findOneIngredient(ingredient.id)
-                        .then(res => {
-                            coffee.ingredients[key] = {
-                                ...ingredient,
-                                ...res
-                            };
-                        });
-                });
-
-                return Promise.all(ingredientPromises).then(() => {
-                    this.setState({
-                        coffee
-                    });
-                });
-            });
-    }
-
-    render () {
-        if (!this.state.coffee) {
-            return (
-                <Container>
-                    <CoffeeName>Coffee Maker</CoffeeName>
-                    <CoffeeCup>
-                        <CupHandle />
-                    </CoffeeCup>
-                </Container>
-            );
-        }
-        
+const Coffee = props => {
+    if (!props.coffee) {
         return (
             <Container>
-                <CoffeeName><Favorite 
-                    onClick={this.props.toggleIsFavorite}
-                    style={{
-                        color: this.props.isFavorite ? '#ffe500' : '#FFFFFF'
-                    }}
-                    className={classnames({
-                        'fa': true,
-                        'fa-heart': this.props.isFavorite,
-                        'fa-heart-o': !this.props.isFavorite
-                    })}
-                /> {this.state.coffee.name}</CoffeeName>
+                <CoffeeName>Coffee Maker</CoffeeName>
                 <CoffeeCup>
                     <CupHandle />
-                    {this.state.coffee.ingredients.map(ingredient => (
-                        <Ingredient key={ingredient.id} style={{
-                            backgroundColor: ingredient.color,
-                            color: ingredient.textColor || '#000000',
-                            height: ingredient.oz * 50
-                        }}>{ingredient.name}</Ingredient>
-                    ))}
                 </CoffeeCup>
-                <CoffeeDescription>{this.state.coffee.description}</CoffeeDescription>
             </Container>
         );
     }
+    
+    return (
+        <Container>
+            <CoffeeName><Favorite 
+                onClick={props.toggleIsFavorite}
+                style={{
+                    color: R.ifElse(
+                        R.equals(true),
+                        R.always('#ffe500')
+                        R.always('#FFFFFF')
+                    )(props.isFavorite)
+                }}
+                className={classnames({
+                    'fa': true,
+                    'fa-heart': props.isFavorite,
+                    'fa-heart-o': !props.isFavorite
+                })}
+            /> {props.name}</CoffeeName>
+            <CoffeeCup>
+                <CupHandle />
+                {props.coffee.ingredients.map(ingredient => (
+                    <Ingredient key={ingredient.id} style={{
+                        backgroundColor: ingredient.color,
+                        color: ingredient.textColor || '#000000',
+                        height: ingredient.oz * 50
+                    }}>{ingredient.name}</Ingredient>
+                ))}
+            </CoffeeCup>
+            <CoffeeDescription>{props.coffee.description}</CoffeeDescription>
+        </Container>
+    );
+};
 
+const getCoffee = props => {
+    if (!props.id) {
+        return;
+    }
+
+    findOneCoffee(props.id)
+        .then(coffee => {
+
+            props.setCoffee(
+                coffee
+            );
+
+            const ingredientPromises = coffee.ingredients.map((ingredient, key) => {
+                return findOneIngredient(ingredient.id)
+                    .then(res => {
+                        coffee.ingredients[key] = {
+                            ...ingredient,
+                            ...res
+                        };
+                    });
+            });
+
+            return Promise.all(ingredientPromises).then(() => {
+                props.setCoffee(
+                    coffee
+                );
+            });
+        });
 }
 
-export default Coffee;
+export default compose(
+    withState('coffee', 'setCoffee', null),
+    componentDidMount(getCoffee)
+)(Coffee);
